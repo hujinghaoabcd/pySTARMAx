f0d9c25f7c131d36bafb47b302b39931456b9ddf
Jinghao Hu <731409213@qq.com>
2026-07-29 10:29:10 +0800

    Export bootstrap-enabled model classes

--- src/pystarmax/models/__init__.py
+++ src/pystarmax/models/__init__.py
@@ -3,8 +3,8 @@
 
 """Model classes."""
 
-from pystarmax.models.seasonal_starima import SeasonalSTARIMA
-from pystarmax.models.starima import STARIMA
-from pystarmax.models.starma import STAR, STARMA
+from pystarmax.models.bootstrap_seasonal import SeasonalSTARIMA
+from pystarmax.models.bootstrap_starima import STARIMA
+from pystarmax.models.bootstrap_starma import STAR, STARMA
 
 __all__ = ["STAR", "STARMA", "STARIMA", "SeasonalSTARIMA"]
